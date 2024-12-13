4aa7db557b733def57d4cd769df65073742c3282
14thDream <dndelara@proton.me>
2024-12-13 15:23:00 +0800

    Added IAccountService interface

---
+++ Managers/Services/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace Managers.Services
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAllAccounts();
+        Account GetAccountById(int id);
+        void AddAccount(Account account);
+        Account? UpdateAccount(int id, Account account);
+        bool DeleteAccount(int id);
+    }
+}
