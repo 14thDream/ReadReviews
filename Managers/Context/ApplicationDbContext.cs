56246004b1616aa536b14ee66dcc814508797ef7
14thDream <dndelara@proton.me>
2024-12-13 15:30:51 +0800

    Added ApplicationDbContext class

---
+++ Managers/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Managers.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<GuestReview> GuestReviews { get; set; }
+        public DbSet<UserReview> UserReviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
