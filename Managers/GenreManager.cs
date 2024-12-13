ce51bd12c8fb1ce9760f8c2ea23c04d702062f27
14thDream <dndelara@proton.me>
2024-12-13 15:49:06 +0800

    Added GenreManager class

---
+++ Managers/GenreManager.cs
@@ -0,0 +1,53 @@
+﻿using Managers.Context;
+using Managers.Services;
+using Models;
+
+namespace Managers
+{
+    public class GenreManager : IGenreService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenreManager(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Genre> GetAllGenres() => _context.Genres.ToList();
+        public Genre? GetGenreById(int id) => _context.Genres.Find(id);
+
+        public void AddGenre(Genre genre)
+        {
+            _context.Genres.Add(genre);
+            _context.SaveChanges();
+        }
+
+        public Genre? UpdateGenre(int id, Genre genre)
+        {
+            var existingGenre = GetGenreById(id);
+            if (existingGenre == null)
+            {
+                return null;
+            }
+
+            existingGenre = genre;
+            _context.SaveChanges();
+
+            return existingGenre;
+        }
+
+        public bool DeleteGenre(int id)
+        {
+            var genre = _context.Genres.Find(id);
+            if (genre == null)
+            {
+                return false;
+            }
+
+            _context.Genres.Remove(genre);
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
