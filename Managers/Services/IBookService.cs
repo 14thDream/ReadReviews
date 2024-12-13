0e9603be3ee5938389d7581ea1d42878bc2afc61
14thDream <dndelara@proton.me>
2024-12-13 15:24:05 +0800

    Add IBookService interface

---
+++ Managers/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace Managers.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAllBooks();
+        Book GetBookById(int id);
+        void AddBook(Book book);
+        Book? UpdateBook(int id, Book book);
+        bool DeleteBook(int id);
+    }
+}
