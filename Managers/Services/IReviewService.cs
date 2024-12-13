c5359e3f011900be9cde6236e89112c9cf664e73
14thDream <dndelara@proton.me>
2024-12-13 15:26:32 +0800

    Added IReviewService interface

---
+++ Managers/Services/IReviewService.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+
+namespace Managers.Services
+{
+    public interface IReviewService
+    {
+        IEnumerable<GuestReview> GetAllGuestReviews();
+        GuestReview GetGuestReviewById(int id);
+        void AddGuestReview(GuestReview guestReview);
+        GuestReview? UpdateGuestReview(int id, GuestReview guestReview);
+        bool DeleteGuestReview(int id);
+
+        IEnumerable<UserReview> GetUserReviews();
+        UserReview GetUserReviewById(int id);
+        void AddUserReview(UserReview userReview);
+        UserReview? UpdateUserReview(int id, UserReview userReview);
+        bool DeleteUserReview(int id);
+    }
+}
