b0c10c20160ddcb34d1379147893cef3cb6de96f
14thDream <dndelara@proton.me>
2024-12-13 15:16:55 +0800

    Added Review model

---
+++ Models/Review.cs
@@ -0,0 +1,20 @@
+﻿namespace Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int Rating { get; set; }
+        public string? Content { get; set; }
+    }
+
+    public class GuestReview : Review
+    {
+        public string Name { get; set; }
+    }
+
+    public class UserReview : Review
+    {
+        public int UserId { get; set; }
+    }
+}
