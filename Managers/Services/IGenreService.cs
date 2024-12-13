d03139f023eb4e941547aec8c963274943c5598c
14thDream <dndelara@proton.me>
2024-12-13 15:21:43 +0800

    Added IGenreService interface

---
+++ Managers/Services/IGenreService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace Managers.Services
+{
+    public interface IGenreService
+    {
+        IEnumerable<Genre> GetAllGenres();
+        Genre GetGenreById(int id);
+        void AddGenre(Genre genre);
+        Genre? UpdateGenre(int id, Genre genre);
+        bool DeleteGenre(int id);
+    }
+}
