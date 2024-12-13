9ac2245e9abafc1394e99318b6dd7bf467e663a6
14thDream <dndelara@proton.me>
2024-12-13 15:11:40 +0800

    Added Account model

---
+++ Models/Account.cs
@@ -0,0 +1,18 @@
+﻿namespace Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public AccountType TypeOfAccount { get; set; }
+    }
+
+    public enum AccountType
+    {
+        User,
+        Moderator,
+        Administrator
+    }
+}
