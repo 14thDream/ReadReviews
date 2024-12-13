420886c3a3098ba73c9427aa529a7928f208f59a
14thDream <dndelara@proton.me>
2024-12-14 01:10:46 +0800

    Connect services and managers with AddScoped

--- ReadReviews/Program.cs
+++ ReadReviews/Program.cs
@@ -1,16 +1,22 @@
+using Managers;
 using Managers.Context;
+using Managers.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IGenreService, GenreManager>();
+builder.Services.AddScoped<IAccountService, AccountManager>();
+builder.Services.AddScoped<IBookService, BookManager>();
+builder.Services.AddScoped<IReviewService, ReviewManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
